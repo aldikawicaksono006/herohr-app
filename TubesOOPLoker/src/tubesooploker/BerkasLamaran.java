c7f1be35fdd844db9fe8b391caec95fc3d4e8bb7
aldikawicaksono006 <aldikawicaksono@gmail.com>
2017-03-10 19:02:01 +0700

    Update-an

---
+++ TubesOOPLoker/src/tubesooploker/BerkasLamaran.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesooploker;
+
+/**
+ *
+ * @author Aldika W
+ */
+
+
+public class BerkasLamaran {
+    //Deklarasi Variabel
+    private int id;
+    private String deksripsi;
+    
+}
